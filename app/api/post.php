4602edfc0d9c84994733e887e36f47bcd4d6820e
IgorTokman <igortokman1994@ukr.net>
2016-04-29 01:15:17 +0300

    implemented the ability to edit and delete existing post

--- app/api/post.php
+++ app/api/post.php
@@ -74,3 +74,60 @@ $app->post('/api/post/add', function (Request $request, Response $response) {
     }
 
 });
+
+//Updates post
+$app->put('/api/post/update/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    $title = $request->getParam('title');
+    $category_id = $request->getParam('category_id');
+    $body = $request->getParam('body');
+
+    $sql = "UPDATE posts SET title = :title, 
+                             category_id = :category_id, 
+                             body = :body 
+                         WHERE id = $id";
+
+    try{
+        //Gets DB object
+        $db = new DB();
+        //Connection
+        $dbConnection = $db->connect();
+
+        $stmt = $dbConnection->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':category_id', $category_id);
+        $stmt->bindParam(':body', $body);
+
+        $stmt->execute();
+
+       echo '{"notice": {"text": "Post ' . $id .' is Updated"}}';
+
+    }catch (PDOException $e){
+        echo '{"error": {"text":' . $e->getMessage() . '}}';
+    }
+
+});
+
+//Deletes post
+$app->delete('/api/post/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    $sql = "DELETE FROM posts WHERE id = $id";
+
+    try{
+        //Gets DB object
+        $db = new DB();
+        //Connection
+        $dbConnection = $db->connect();
+
+        $stmt = $dbConnection->prepare($sql);
+        $stmt->execute();
+
+       echo '{"notice": {"text": "Post ' . $id .' is Deleted"}}';
+
+    }catch (PDOException $e){
+        echo '{"error": {"text":' . $e->getMessage() . '}}';
+    }
+
+});
